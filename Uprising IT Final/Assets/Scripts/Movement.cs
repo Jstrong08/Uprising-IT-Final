bca38230cdbbec7b8f0e23e501348d7c75a263a4
Jstrong08 <39315221+Jstrong08@users.noreply.github.com>
2018-05-16 12:58:52 -0700

    movement

--- Uprising IT Final/Assets/Scripts/Movement.cs
+++ Uprising IT Final/Assets/Scripts/Movement.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour {
 
-	// Use this for initialization
+    public float speed;
+
+    private Rigidbody2D rb2d;
+
+	
 	void Start () {
+
+         rb2d = GetComponent<Rigidbody2D> ();
+        
 		
 	}
 	
-	// Update is called once per frame
+	
 	void Update () {
-		
-	}
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+        
+        rb2d.AddForce(movement * speed);
+
+
+    }
 }
